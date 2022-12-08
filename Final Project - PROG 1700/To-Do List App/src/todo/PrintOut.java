7917a1c10f68d2fc722bb98e66dde765860dd66c
Ian Christian <112981727+IChrist-dev@users.noreply.github.com>
2022-12-08 14:11:50 -0400

    Delete Final Project - PROG 1700 directory

--- Final Project - PROG 1700/To-Do List App/src/todo/PrintOut.java
+++
@@ -1,8 +0,0 @@
-package todo;
-
-public class PrintOut {
-    public void printTask(Task printableTask) {
-        System.out.printf("%-20s %-20s %-20s %-20s\n", printableTask.getTitle(),
-                printableTask.getDueDate(), printableTask.getStatus(), printableTask.getDesc());
-    }
-}
